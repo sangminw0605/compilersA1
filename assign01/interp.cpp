60ecd20ad1ff8d8d032e25948263d46469215530
sangminw0605 <swoo17@jhu.edu>
2022-09-26 17:35:54 -0400

    some comments

--- assign01/interp.cpp
+++ assign01/interp.cpp
@@ -64,24 +64,31 @@ void Interpreter::analyze_recurse(Node *ast, Environment *env)
 Value Interpreter::execute()
 {
   // TODO: implement
+
+  // Global environment
   Environment *global_env = new Environment();
 
+  // Define intrinsic functions
   global_env->define("print");
   global_env->define("println");
 
+  // Bind intrinsic functions
   global_env->assign("print", &intrinsic_print);
   global_env->assign("println", &intrinsic_println);
 
+  // Evaluates each statement
   for (unsigned int i = 0; i < m_ast->get_num_kids() - 1; i++)
   {
     ex(m_ast->get_kid(i), global_env);
   }
 
+  // Return for result
   return ex(m_ast->get_last_kid(), global_env);
 }
 
 Value Interpreter::ex(Node *ast, Environment *env)
 {
+  // Execute each statement in a block of statements
   if (ast->get_tag() == AST_STATEMENT_LIST)
   {
     for (unsigned int i = 0; i < ast->get_num_kids(); i++)
@@ -91,90 +98,127 @@ Value Interpreter::ex(Node *ast, Environment *env)
     return 0;
   }
 
+  // Call function
   if (ast->get_tag() == AST_FNCALL)
   {
+    // Find the function definition
     Environment *location = findEnv(ast, env);
 
+    // Get args the the program entered
     unsigned numargs = ast->get_kid(0)->get_num_kids();
     Value args[numargs];
 
+    // Evaluate each arg
     for (unsigned int i = 0; i < ast->get_kid(0)->get_num_kids(); i++)
     {
       args[i] = ex(ast->get_kid(0)->get_kid(i), env);
     }
 
+    // Retrieve the function
     IntrinsicFn fn = (location->lookup(ast->get_str())).get_intrinsic_fn();
+
+    // Execute the function
     return fn(args, numargs, ast->get_loc(), this);
   }
 
+  // Is statement
   if (ast->get_tag() == AST_STATEMENT)
   {
     return ex(ast->get_kid(0), env);
   }
 
+  // Is int literal
   if (ast->get_tag() == AST_INT_LITERAL)
   {
     return atoi(ast->get_str().c_str());
   }
 
+  // Vardef
   if (ast->get_tag() == AST_DEFINITION)
   {
     env->define(ast->get_kid(0)->get_str());
     return 0;
   }
 
+  // Var assignment
   if (ast->get_tag() == AST_ASSIGNMENT)
   {
+    // Find correct environment
     Environment *location = findEnv(ast->get_kid(0), env);
+
+    // Assign in the appropriate environment
     location->assign(ast->get_kid(0)->get_str(), ex(ast->get_kid(1), env));
+
+    // Return assignment value
     return location->lookup(ast->get_kid(0)->get_str());
   }
 
+  // Var reference
   if (ast->get_tag() == AST_VARREF)
   {
+    // Find correct environment
     Environment *location = findEnv(ast, env);
+
+    // Retrieve variable value
     return location->lookup(ast->get_str());
   }
 
+  // If statement
   if (ast->get_tag() == AST_IF)
   {
+    // Check if the condition actually produces a number
     if (non_numeric(ast->get_kid(0), env))
     {
       EvaluationError::raise(ast->get_loc(), "Non-numeric condition");
     }
+
+    // Check if condition evaluates to true
     if (ex(ast->get_kid(0), env).get_ival() != 0)
     {
+      // Create new block
       Environment *block_env = new Environment(env);
       ex(ast->get_kid(1), block_env);
     }
+
+    // Execute else block if above not trigered
     else if (ast->get_last_kid()->get_tag() == AST_ELSE)
     {
+      // Create new block
       Environment *block_env = new Environment(env);
       ex(ast->get_last_kid()->get_kid(0), block_env);
     }
     return 0;
   }
 
+  // While loop
   if (ast->get_tag() == AST_WHILE)
   {
+    // Check if the condition actually produces a number
     if (non_numeric(ast->get_kid(0), env))
     {
       EvaluationError::raise(ast->get_loc(), "Non-numeric condition");
     }
+
+    // Execute body while condition evaluates to true
     while (ex(ast->get_kid(0), env).get_ival() != 0)
     {
+      // Create new block
       Environment *block_env = new Environment(env);
       ex(ast->get_kid(1), block_env);
     }
     return 0;
   }
 
+  // Check if operand1 is numeric
   if (non_numeric(ast->get_kid(0), env))
   {
     EvaluationError::raise(ast->get_loc(), "Non-numeric condition");
   }
+
+  // Retrieve first operand
   int val1 = (ex(ast->get_kid(0), env)).get_ival();
 
+  // Short circuit &&
   if (ast->get_tag() == AST_LOGICAL_AND)
   {
     if (val1 == 0)
@@ -182,6 +226,7 @@ Value Interpreter::ex(Node *ast, Environment *env)
       return 0;
     }
   }
+  // Short circuit ||
   else if (ast->get_tag() == AST_LOGICAL_OR)
   {
     if (val1 != 0)
@@ -190,13 +235,16 @@ Value Interpreter::ex(Node *ast, Environment *env)
     }
   }
 
+  // Check if operand2 is numeric
   if (non_numeric(ast->get_kid(1), env))
   {
     EvaluationError::raise(ast->get_loc(), "Non-numeric condition");
   }
 
+  // Retrieve second operand
   int val2 = (ex(ast->get_kid(1), env)).get_ival();
 
+  // Perform associated operation
   return doOp(ast->get_tag(), val1, val2, ast->get_kid(1));
 }
 
